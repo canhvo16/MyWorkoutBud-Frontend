3eee0e126339a188289850626e91ed74924038fa
canhvo16 <canhvo16@gmail.com>
2022-05-15 16:32:53 -0500

    deploy

--- src/services/index.js
+++ src/services/index.js
@@ -1,6 +1,6 @@
 import Axios from 'axios'
 
-export const BASE_URL = 'http://localhost:3001/api'
+export const BASE_URL = 'https://myworkoutbud-backend.herokuapp.com/api'
 
 const Client = Axios.create({ baseURL: BASE_URL })
 
